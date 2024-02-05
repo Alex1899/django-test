040688d78f15420da013cd5dc036c1d3be41d50c
Alex Kvashilava <alex.codes.101@gmail.com>
2024-02-05 06:41:38 +0000

    chore: update readme

--- wordpuzzle/api/apps.py
+++ wordpuzzle/api/apps.py
@@ -1,4 +1,6 @@
+import time
 from django.apps import AppConfig
+from halo import Halo
 from .word_loader import WordLoader
 
 
@@ -7,4 +9,22 @@ class ApiConfig(AppConfig):
     name = "api"
 
     def ready(self):
-        WordLoader.load_words()
+        start_time = time.time()
+        spinner = Halo(text="Loading dictionary words...", spinner="dots")
+        spinner.start()
+
+        try:
+            WordLoader.load_words()
+
+            end_time = time.time()
+            total_time = end_time - start_time
+
+            spinner.stop_and_persist(
+                symbol="✅".encode("utf-8"),
+                text=f"Loaded words in {round(total_time, 1)} seconds",
+            )
+
+        except Exception as e:
+            spinner.stop_and_persist(
+                symbol="❌".encode("utf-8"), text=f"Error while loading words: {e}"
+            )
--- wordpuzzle/api/word_loader.py
+++ wordpuzzle/api/word_loader.py
@@ -1,7 +1,5 @@
-import time
 from collections import defaultdict
 from django.conf import settings
-from halo import Halo
 
 
 class WordLoader:
@@ -18,37 +16,16 @@ class WordLoader:
         For each word, it generates "wildcard words" by replacing each letter of the word with a "*",
         and maps these wildcard words to the original word in the neighbors dictionary. This is used to
         quickly find all words that can be formed by changing one letter of a given word.
-
+        
         """
 
-        start_time = time.time()
-        # Start the loading spinner in a separate thread
-        spinner = Halo(text="Loading dictionary words...", spinner="dots")
-        spinner.start()
-
-        try:
-            start_time = time.time()
-
-            with open(settings.WORDS_FILE_PATH, "r", encoding="utf-8") as file:
-                for line in file:
-                    word = line.strip()
-                    cls.words.add(word)
-                    for i in range(len(word)):
-                        wildcard_word = word[:i] + "*" + word[i + 1 :]
-                        cls.neighbours[wildcard_word].append(word)
-
-            end_time = time.time()
-            total_time = end_time - start_time
-
-            spinner.stop_and_persist(
-                symbol="✅".encode("utf-8"),
-                text=f"Loaded words in {round(total_time, 1)} seconds",
-            )
-
-        except Exception as e:
-            spinner.stop_and_persist(
-                symbol="❌".encode("utf-8"), text=f"Error while loading words: {e}"
-            )
+        with open(settings.WORDS_FILE_PATH, "r", encoding="utf-8") as file:
+            for line in file:
+                word = line.strip()
+                cls.words.add(word)
+                for i in range(len(word)):
+                    wildcard_word = word[:i] + "*" + word[i + 1 :]
+                    cls.neighbours[wildcard_word].append(word)
 
     @classmethod
     def get_word_set(cls):
